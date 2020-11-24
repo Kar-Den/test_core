cbea96841c48f8952c3272091452970ab70356aa
Kar-Den <k.denis.vlad@gmail.com>
2020-11-24 15:34:48 +0300

    переименовал figureList в capacityList

--- src/by/itacademy/Main.java
+++ src/by/itacademy/Main.java
@@ -12,58 +12,54 @@ import by.itacademy.type.FluidType;
 import java.util.*;
 
 public class Main {
+
     public static void main(String[] args) {
-        List<Capacity> figureList = new ArrayList<>();
+        List<Capacity> capacityList = new ArrayList<>();
 
         Capacity squareStraight = new SquareStraight(2L, FluidType.MACHINE_OIL);
-        figureList.add(squareStraight);
+        capacityList.add(squareStraight);
 
         Capacity squareStraight1 = new SquareStraight(3L, FluidType.MACHINE_OIL);
-        figureList.add(squareStraight1);
+        capacityList.add(squareStraight1);
 
         Capacity squareStraight2 = new SquareStraight(3L, FluidType.MACHINE_OIL);
-        figureList.add(squareStraight2);
+        capacityList.add(squareStraight2);
 
         Capacity squareInclined = new SquareInclined(2L, 3L, FluidType.PETROL);
-        figureList.add(squareInclined);
+        capacityList.add(squareInclined);
 
         Capacity rectangleStraight = new RectangleStraight(2L, 3L, FluidType.KEROSENE);
-        figureList.add(rectangleStraight);
+        capacityList.add(rectangleStraight);
 
         Capacity isoscelesTrapezoidStraight = new IsoscelesTrapezoidStraight(2L, 3L, 2L, FluidType.MACHINE_OIL);
-        figureList.add(isoscelesTrapezoidStraight);
+        capacityList.add(isoscelesTrapezoidStraight);
 
         Capacity isoscelesTrapezoidInclined = new IsoscelesTrapezoidInclined(2L, 3L, 2L, 3L, 4L, 2L, FluidType.MACHINE_OIL);
-        figureList.add(isoscelesTrapezoidInclined);
+        capacityList.add(isoscelesTrapezoidInclined);
 
         Capacity regularHexagonStraight = new RegularHexagonStraight(2L, FluidType.PETROL);
-        figureList.add(regularHexagonStraight);
+        capacityList.add(regularHexagonStraight);
 
         Capacity regularHexagonInclined = new RegularHexagonInclined(2L, 3L, FluidType.KEROSENE);
-        figureList.add(regularHexagonInclined);
+        capacityList.add(regularHexagonInclined);
 
         Capacity circleStraight = new CircleStraight(2L, FluidType.MACHINE_OIL);
-        figureList.add(circleStraight);
+        capacityList.add(circleStraight);
 
         Capacity circleInclined = new CircleInclined(2L, 3L, FluidType.PETROL);
-        figureList.add(circleInclined);
+        capacityList.add(circleInclined);
 
 
-        figureList.sort(new CapacityComparator());
-        figureList.forEach(System.out::println);
+        CapacityComparator capacityComparator = new CapacityComparator();
+        capacityList.sort(capacityComparator);
+        capacityList.forEach(System.out::println);
 
         System.out.println();
         System.out.println("наибольшая масса керосина:");
-        double maxMassKerosene = 0;
-        double massTemp;
-        for (Capacity item : figureList){
-            if (item.isKerosene()){
-                massTemp=item.mass();
-                if (massTemp>maxMassKerosene){
-                    maxMassKerosene = massTemp;
-                }
-            }
-        }
-        System.out.println(maxMassKerosene);
+
+        capacityList.stream()
+                .filter(Capacity::isKerosene)
+                .max(capacityComparator)
+                .ifPresent(capacity -> System.out.println(capacity.mass()));
     }
 }
